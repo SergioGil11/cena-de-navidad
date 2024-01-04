325bc38234e36b70e6a0a659a73a6843c38144f3
Sergio Andrés Gil Garnica <151978735+SergioGil11@users.noreply.github.com>
2024-01-03 21:06:03 -0500

    suma_tresNumeros.py

--- trabajo cena navideña/suma_Numeros.py
+++ trabajo cena navideña/suma_Numeros.py
@@ -3,8 +3,8 @@ def sumar_numeros():
         try:
             n1 = int(input("Por favor, ingrese el primer número que desea sumar: "))
             n2 = int(input("Por favor, ingrese el segundo número que desea sumar: "))
-            suma = n1 + n2
-            print("El resultado de la suma de los dos números es:", suma)
+            suma = n1 + n2 + n3 
+            print("El resultado de la suma de los tres números es:", suma)
             return suma
         except ValueError:
             print("El valor que ingresó no es un número. Por favor, ingrese un número e intente de nuevo.")
